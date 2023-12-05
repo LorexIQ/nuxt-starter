ad586c20ab4109ac95f2729be7e7c3ca373d8a4c
LorexIQ <murashko100500@gmail.com>
2023-12-06 01:21:00 +0300

    feat: add env data to runtimeConfig

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,21 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+const packageJSON = require('./package.json');
+
+const env = {
+};
+
 export default defineNuxtConfig({
     ssr: false,
+
+    runtimeConfig: {
+        public: {
+            appVersion: packageJSON.version,
+            ...env
+        }
+    },
 
     css: [
         '~/assets/styles/main.scss'
     ],
 
-    devtools: {enabled: true}
+    devtools: { enabled: true }
 })
